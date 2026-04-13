cca7ed871faa3aaa8e613ec5fb4af2dc5e17b399
Samuel Soumi <samuel_somy93@hotmail.com>
2026-04-13 13:58:43 +0200

    feat: implement caching for pokemon details to improve performance

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -11,13 +11,22 @@ export const fetchPokemonList = async () => {
     return json.results;
 }
 
+const pokemonDetailsCache = {};
+
 export const fetchPokemonDetails = async (pokemonName) => {
+    if (pokemonDetailsCache[pokemonName]) {
+        return pokemonDetailsCache[pokemonName];
+    }
+
     const response = await fetch(`${BASE_URL}/pokemon/${pokemonName}`);
 
     if (!response.ok) {
         throw new Error('Could not fetch pokemon details')
     }
 
-    return response.json();
+    const data = await response.json();
+    pokemonDetailsCache[pokemonName] = data;
+
+    return data;
 }
 
